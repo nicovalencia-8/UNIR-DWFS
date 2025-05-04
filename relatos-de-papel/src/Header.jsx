eb14384e9d33f7ba80654da36065750bc8dcfcd9
Sergio David Muñoz Vela <velamds@gmail.com>
2025-05-04 13:44:23 -0500

    Inicio Actividad 1 relatos de papel

---
+++ relatos-de-papel/src/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div>Icono</div>
+        <div>Buscador</div>
+        <div>Icono Carrito</div>
+        <div>Icono Corazón</div>
+        <div>Icono Usuario</div>
+        <div>Icono Lengauje</div>
+    </div>
+  )
+}
+
+export default Header
