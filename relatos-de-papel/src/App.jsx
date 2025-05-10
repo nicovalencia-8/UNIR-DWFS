9338d55e7ba57c3d8fcec389865905322320052c
Sergio David Muñoz Vela <velamds@gmail.com>
2025-05-09 20:34:03 -0500

    añadido navbar y footer, el content es de prueba

--- relatos-de-papel/src/App.jsx
+++ relatos-de-papel/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Header from './Header'
+import Navbar from "./Navbar.jsx";
+import Footer from "./Footer.jsx";
 
 const App = () => {
   return (
     <div>
       <Header></Header>
+      <Navbar></Navbar>
+        <p className="content">Contenido de Ejemplo</p>
+      <Footer></Footer>
     </div>
   )
 }
---
+++ relatos-de-papel/src/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar__item">
+                Inicio
+            </div>
+            <div className="navbar__item">
+                Categorías
+            </div>
+            <div className="navbar__item">
+                Acerca De
+            </div>
+            <div className="navbar__item">
+                Contacto
+            </div>
+
+
+        </nav>
+    )
+}
+
+export default Navbar
